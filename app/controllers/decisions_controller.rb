401563e8edd592bbc60c7e859cf7bf8f28cb14be
Alan Norton <git@nonration.al>
2021-08-01 09:10:26 -0400

    gh-pages only

--- app/controllers/decisions_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class DecisionsController < ApplicationController
-  def index
-    @decisions = Decision.all
-  end
-end
--- app/models/decision.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class Decision < ApplicationRecord
-end
